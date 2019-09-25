119c2aef82470417952b4ac09dda3f04070a3a9b
Parker Thompson <Parker.B.Thompson-1@ou.edu>
2019-09-25 11:33:33 -0500

    participation

---
+++ jsonexample/jsonexample/GOTquote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsonexample
+{
+    class GOTquote
+    {
+        public string quote { get; set; }
+
+        public string character { get; set; }
+    }
+}
---
+++ jsonexample/jsonexample/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jsonexample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetquote_Click(object sender, RoutedEventArgs e)
+        {
+            //https://got-quotes.herokuapp.com/quotes
+
+            using (HttpClient client = new HttpClient())//using system.net.http
+            {
+                HttpResponseMessage response = client.GetAsync(@"https://got-quotes.herokuapp.com/quotes").Result;//need @ when entering url .result gets out of Asynch. 
+                if (response.IsSuccessStatusCode)//
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    GOTquote gq = JsonConvert.DeserializeObject<GOTquote>(content); //using newtonsoft.json
+
+                   // var x = JsonConvert.SerializeObject(gq); not needed
+                   txtQuote.Text = gq.quote + "\n" + "-" + gq.character;
+                }
+            }
+            //copied and pasted from canvas
+        }
+    }
+}
